f5bd2178adffb882b6c3a7ec238f8990c8dcc7ab
ouali <ouali@res-PC.artesrenault.lan>
2015-10-30 19:49:51 +0100

    Signed-off-by: ouali <ouali@res-PC.artesrenault.lan>

---
+++ eMarket-ejb/src/main/java/services/module1/impl/C.java
@@ -0,0 +1,21 @@
+package services.module1.impl;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: C
+ *
+ */
+@Entity
+
+public class C implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public C() {
+		super();
+	}
+   
+}
