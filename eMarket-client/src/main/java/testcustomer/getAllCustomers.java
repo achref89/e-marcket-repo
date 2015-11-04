0adbbf1e9a14dc38a5c3be6be06144573c055481
ouali <ouali@res-PC.artesrenault.lan>
2015-11-04 23:02:55 +0100

    Signed-off-by: ouali <ouali@res-PC.artesrenault.lan>

---
+++ eMarket-client/src/main/java/testcustomer/getAllCustomers.java
@@ -0,0 +1,26 @@
+package testcustomer;
+
+
+
+
+	import javax.naming.Context;
+	import javax.naming.InitialContext;
+	import javax.naming.NamingException;
+
+	import entities.Customer;
+	import entities.Product;
+	import services.module1.interfaces.CustomerServiceRemote;
+	import services.module2.interfaces.ProductServiceRemote;
+
+	public class getAllCustomers {
+
+
+		public static void main(String[] args) throws NamingException {
+			Context context = new InitialContext();
+			CustomerServiceRemote proxy = (CustomerServiceRemote) context
+					.lookup("/eMarket-ejb/CustomerService!services.module2.interfaces.CustomerServiceRemote");
+
+					Customer customer =new Customer();
+		    proxy.getAllCustomers();
+		}
+	}
